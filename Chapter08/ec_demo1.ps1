b24f6036b65ede7ca496f4ace1dfb5344f74bbbc
sagarsawant69 <sagarsawant@packtpub.com>
2018-03-28 13:26:53 +0530

    Code files added

---
+++ Chapter08/ec_demo1.ps1
@@ -0,0 +1,23 @@
+﻿# Change your EC Token here
+$token = '49CB0FF2-C685-4277-B744-6550516175CF'
+
+# Disable SSL Validation
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+# Create a dictionary object to contain the HTTP headers
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+
+# Create an HTTP Authorization header with the EC Token
+$headers.Add("Authorization", 'Splunk ' + $token)
+
+# Create the JSON data that will be sent along with the POST request
+$event = @{
+    event="demo event 1"
+}
+$json = $event | ConvertTo-Json
+
+# Initiate the POST request including the headers and the JSON payload
+$response = Invoke-RestMethod 'http://localhost:8088/services/collector' -Method Post -Body $json -ContentType 'application/json' -Headers $headers
+
+# Echo the response
+Write-Host $response
